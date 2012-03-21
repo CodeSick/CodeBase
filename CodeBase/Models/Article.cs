81964997ea3a0b8958536aa1145fc68309d14690
Gregor Panic <vision810@gmail.com>
2012-03-21 17:13:09 +0100

    init

---
+++ CodeBase/Models/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeBase.Models
+{
+    public class Article
+    {
+        [Key]
+        public int id { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+
+    }
+}
