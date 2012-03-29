b7b86f30e8f0bd11a9618e89dde1b008b2d24ecf
Snuderl <Snuderl@gmail.com>
2012-03-29 10:20:25 +0200

    Added autocomplete search, chrome malo jebe

---
+++ CodeBase/Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeBase.Models;
+
+namespace CodeBase.Controllers
+{
+    public class SearchController : Controller
+    {
+        CodeBaseContext context = new CodeBaseContext();
+        //
+        // GET: /Search/
+
+        public ActionResult SearchArticlesJson(String data)
+        {
+            return Json(context.Articles.Where(x => x.Title.ToLower().Contains(data)).Take(5).Select(x => new { name = x.Title, id = x.ArticleId }), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
