5611f9cf2304b226466c7e55afdbf027d8a313ad
Snuderl <Snuderl@gmail.com>
2012-04-09 17:21:48 +0200

    Revert "Merge"
    This reverts commit ef62ec654d1d5c4ae6d793e558de24c7a8e2eb97.

---
+++ CodeBase/Models/ICodeBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CodeBase.Models
+{
+    public interface ICodeBaseRepository
+    {
+        System.Data.Entity.IDbSet<Answer> Answers { get; set; }
+        System.Data.Entity.IDbSet<Article> Articles { get; set; }
+        System.Data.Entity.IDbSet<Category> Categories { get; set; }
+        System.Data.Entity.IDbSet<Comment> Comments { get; set; }
+        System.Data.Entity.IDbSet<File> Files { get; set; }
+        System.Data.Entity.IDbSet<Question> Questions { get; set; }
+        System.Data.Entity.IDbSet<Rating> Ratings { get; set; }
+        System.Data.Entity.IDbSet<User> Users { get; set; }
+    }
+}
