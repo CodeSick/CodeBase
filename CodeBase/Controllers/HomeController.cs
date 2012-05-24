7e0fd57884f365ff7fad286cff7d3f9c290c0be9
mantic0 <jernej.legvart@gmail.com>
2012-05-24 15:10:43 +0200

    Front page modified

--- CodeBase/Controllers/HomeController.cs
+++ CodeBase/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using CodeBase.Models;
 using CodeBase.ViewModel;
 using System.Data.Entity;
+using CodeBase.Helper;
 
 namespace CodeBase.Controllers
 {
@@ -17,10 +18,12 @@ namespace CodeBase.Controllers
         {
             IndexViewModel model = new IndexViewModel 
             { Message = "Hello to this beautiful site",
-                Articles = context.Articles.Where(x => x.Approved==true).Take(5).ToList(),
-                Users= context.Users.OrderByDescending(x => x.Articles.Count).Take(5).Select(x => new UserWithCount{ User=x, Count=x.Articles.Count})
+                Articles = context.Articles.Where(x => x.Approved==true).Take(5).ToList().OrderByDescending(x => x.Date),
+                Users= context.Users.OrderByDescending(x => x.Articles.Count).Take(5).Select(x => new UserWithCount{ User=x, Count=x.Articles.Count}),
+                Questions = context.Questions.OrderByDescending(x => x.Answers.Count).Take(5).Select(x => new QuestionsWithCount{ Question = x, Count = x.Answers.Count }),
+                ArticlesRating = context.Articles.OrderByDescending(x => x.Ratings.Sum( r => r.Value)/ x.Ratings.Count).Select(x => new ArticleRating { Article = x }).Take(5)
             };
-
+            
             return View("Index",model);
         }
 
--- CodeBase/ViewModel/IndexViewModel.cs
+++ CodeBase/ViewModel/IndexViewModel.cs
@@ -11,11 +11,25 @@ namespace CodeBase.ViewModel
         public String Message { get; set; }
         public IEnumerable<Article> Articles { get; set; }
         public IEnumerable<UserWithCount> Users { get; set; }
+        public IEnumerable<QuestionsWithCount> Questions { get; set; }
+        public IEnumerable<ArticleRating> ArticlesRating { get; set; }
     }
 
     public class UserWithCount
     {
         public User User { get; set; }
         public int Count { get; set; }
+    }
+
+    public class QuestionsWithCount
+    {
+        public Question Question { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class ArticleRating
+    {
+        public Article Article { get; set; }
+        public float Rating { get; set; }
     }
 }
